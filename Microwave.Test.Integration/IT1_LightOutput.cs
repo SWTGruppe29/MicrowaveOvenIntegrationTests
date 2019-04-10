360fcfa7cc5a49097afb2cdcc82f991f776c545c
Mathias Nielsen <mathiasnielsen0@gmail.com>
2019-04-10 11:37:28 +0200

    IntegrationProj

---
+++ Microwave.Test.Integration/IT1_LightOutput.cs
@@ -0,0 +1,17 @@
+﻿using MicrowaveOvenClasses.Boundary;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class IT1_LightOutput
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+    }
+}
