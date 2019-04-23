af04996d60360da2a2f4418c3d4a1b33c39d8b84
Mathias <math084m@hotmail.com>
2019-04-23 13:21:09 +0200

    PowerOuts

--- Microwave.Integration.Test/IT03_PowerOut.cs
+++ Microwave.Integration.Test/IT03_PowerOut.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
 
@@ -9,5 +12,66 @@ namespace Microwave.Test.Integration
     [TestFixture]
     public class IT03_PowerOut
     {
+
+        private PowerTube uut;
+        private IOutput output;
+
+        [SetUp]
+        public void Setup()
+        {
+            output = new Output();
+            uut = new PowerTube(output);
+        }
+
+        [Test]
+        public void TurnOn_WasOff_CorrectOutput()
+        {
+            uut.TurnOn(50);
+            //output.Received().OutputLine(Arg.Is<string>(str => str.Contains("50 %")));
+        }
+
+        [Test]
+        public void TurnOff_WasOn_CorrectOutput()
+        {
+            uut.TurnOn(50);
+            uut.TurnOff();
+            //output.Received().OutputLine(Arg.Is<string>(str => str.Contains("off")));
+        }
+
+        [Test]
+        public void TurnOff_WasOff_NoOutput()
+        {
+            uut.TurnOff();
+            //output.DidNotReceive().OutputLine(Arg.Any<string>());
+        }
+
+        [Test]
+        public void TurnOn_WasOn_ThrowsException()
+        {
+            uut.TurnOn(50);
+            Assert.Throws<System.ApplicationException>(() => uut.TurnOn(60));
+        }
+
+        [Test]
+        public void TurnOn_NegativePower_ThrowsException()
+        {
+            Assert.Throws<System.ArgumentOutOfRangeException>(() => uut.TurnOn(-1));
+        }
+
+        [Test]
+        public void TurnOn_HighPower_ThrowsException()
+        {
+            Assert.Throws<System.ArgumentOutOfRangeException>(() => uut.TurnOn(101));
+        }
+
+        [Test]
+        public void TurnOn_ZeroPower_ThrowsException()
+        {
+            Assert.Throws<System.ArgumentOutOfRangeException>(() => uut.TurnOn(0));
+        }
+
+
+
+
     }
 }
