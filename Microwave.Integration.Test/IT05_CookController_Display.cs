f663dbce8008e11c4a3ec2107a41e7e62f15f64e
Mathias Nielsen <mathiasnielsen0@gmail.com>
2019-04-23 13:49:33 +0200

    I05

--- Microwave.Integration.Test/IT05_CookController_Display.cs
+++ Microwave.Integration.Test/IT05_CookController_Display.cs
@@ -1,14 +1,32 @@
-﻿using NUnit.Framework;
+﻿using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
 
 namespace Microwave.Integration.Test
 {
     [TestFixture]
     public class IT05_CookController_Display
     {
+        private CookController uut;
+
+        private IUserInterface ui;
+        private ITimer timer;
+        private IDisplay display;
+        private IPowerTube powerTube;
+        private IOutput output;
+
         [SetUp]
         public void Setup()
         {
+            ui = Substitute.For<IUserInterface>();
+            powerTube = Substitute.For<IPowerTube>();
+            output = new Output();
+            display = new Display(output);
+            timer = new Timer();
 
+            uut = new CookController(timer, display, powerTube, ui);
         }
 
         [Test]
