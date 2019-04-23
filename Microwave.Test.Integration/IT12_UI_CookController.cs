6ccf2e1233c42f3054cfd7ac8e01b885dd3515bf
Mathias <math084m@hotmail.com>
2019-04-23 12:46:31 +0200

    Interationclasses made 8-12

---
+++ Microwave.Test.Integration/IT12_UI_CookController.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microwave.Test.Integration
+{
+    class IT12_UI_CookController
+    {
+    }
+}
---
+++ Microwave.Test.Integration/IT8_UI_Door.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microwave.Test.Integration
+{
+    class IT8_UI_Door
+    {
+    }
+}
