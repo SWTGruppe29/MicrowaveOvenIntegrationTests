4fb4630237cd18b53ed1e37081a019b4fa69a031
Mathias <math084m@hotmail.com>
2019-04-23 12:51:15 +0200

    setup

--- Microwave.Test.Integration/IT10_UI_Light.cs
+++ Microwave.Test.Integration/IT10_UI_Light.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
 
 namespace Microwave.Test.Integration
 {
+    [TestFixture]
     class IT10_UI_Light
     {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+
+        }
     }
 }
--- Microwave.Test.Integration/IT8_UI_Door.cs
+++ Microwave.Test.Integration/IT8_UI_Door.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using NUnit.Framework;
 
 namespace Microwave.Test.Integration
 {
+    [TestFixture]
     class IT8_UI_Door
     {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+
+        }
     }
 }
