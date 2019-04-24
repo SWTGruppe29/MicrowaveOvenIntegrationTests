c36cab73db121423efcc0ce2869020219f550584
Mathias <math084m@hotmail.com>
2019-04-24 11:32:59 +0200

    UI_CookController IT11

--- Microwave.Integration.Test/IT11_UI_CookController.cs
+++ Microwave.Integration.Test/IT11_UI_CookController.cs
@@ -1,20 +1,97 @@
-﻿using NUnit.Framework;
+﻿using System.Security.Cryptography.X509Certificates;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
 
 namespace Microwave.Integration.Test
 {
     [TestFixture]
     public class IT11_UI_CookController
     {
+        private UserInterface ui;
+
+        private IButton powerButton;
+        private IButton timeButton;
+        private IButton startCancelButton;
+
+        private IDoor door;
+
+        private IDisplay display;
+        private ILight light;
+
+        private IOutput output;
+        private ICookController cooker;
+
+        private ITimer timer;
+        private IPowerTube powerTube;
+
         [SetUp]
         public void Setup()
         {
+            powerButton = new Button();
+            timeButton = new Button();
+            startCancelButton = new Button();
+            door = new Door();
+            output = new Output();
+            timer = new Timer();
+            powerTube = new PowerTube(output);
+            light = new Light(output);
+            display = new Display(output);
+            cooker = new CookController(timer,display,powerTube);
+
+            ui = new UserInterface(
+                powerButton, timeButton, startCancelButton,
+                door, display, light, cooker);
+        }
+
+        [Test]
+        public void StateSetTime_StartButton_StartCooking()
+        {
+            powerButton.Press();
+            timeButton.Press();
+            startCancelButton.Press();
 
         }
 
         [Test]
-        public void Test1()
+        public void StateReady_PowerAndTime_StartCooking()
         {
+            powerButton.Press();
+            powerButton.Press();
+            timeButton.Press();
+            timeButton.Press();
+            startCancelButton.Press();
+        }
 
+        [Test]
+        public void StateReady_FullPower_StartCooking()
+        {
+            for (int i = 50; i <= 700; i+=50)
+            {
+                powerButton.Press();
+            }
+            timeButton.Press();
+            startCancelButton.Press();
+        }
+
+        [Test]
+        public void StateCooking_DoorIsOpened_CookerStopped()
+        {
+            powerButton.Press();
+            timeButton.Press();
+            startCancelButton.Press();
+            door.Open();
+        }
+
+        [Test]
+        public void StateCooking_CancelButton_CookerStopped()
+        {
+            powerButton.Press();
+            timeButton.Press();
+            startCancelButton.Press();
+            startCancelButton.Press();
         }
     }
 }
